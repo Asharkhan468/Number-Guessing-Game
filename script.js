a02e8907e45db8c44e992178e9beaacdcd416e0a
Ashar Khan <asharullah.khan2468@gmail.com>
2024-04-24 00:04:11 +0500

    Number Guessing Game

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 let input = document.querySelector("#user-input");
 let paragarph = document.querySelector(".para");
 
-let attempt=0;
-attempt++;
 
 function userNum() {
 
-  let num = Math.round(Math.random() * 10 + 1);
+  let num = Math.round(Math.random() * 9 + 1 );
 
   console.log(num);
 
 
   if (input.value == "") {
-    paragarph.innerHTML = "Please Enter Any Number";
+    paragarph.innerHTML = `Please Enter Any Number`;
+    
   } else if (num > input.value) {
-    paragarph.innerHTML = `Too Low ! Try Again`;
-    input.value = "";
+    paragarph.innerHTML = `Too Low ! Try Again <br>
+    <button onclick=" playAgain()" type="button" class="btn btn-danger playagain-btn">
+      Try Again
+    </button>`;
   }else if(num==input.value){
-    paragarph.innerHTML = `Congratulations! You guessed the correct number! 🎉`;  
-    input.value="";
+    paragarph.innerHTML = `Congratulations! You guessed the correct number! 🎉 <br> <button onclick=" playAgain()" type="button" class="btn btn-primary playagain-btn">
+      Play Again
+    </button>`;  
   }else{
-    paragarph.innerHTML = `Too High ! Try Again`;
-    input.value = "";
+    paragarph.innerHTML = `Too High ! Try Again <br> <button onclick=" playAgain()" type="button" class="btn btn-danger playagain-btn">
+      Try Again
+    </button>`;
   } 
 
 
+}
+
+
+function playAgain(){
+  paragarph.innerHTML = "";
+   input.value = "";
 }
 
 
